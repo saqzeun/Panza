e322f21b1b3592e84521886db0f85b47a4d83419
saqzeun <saqzeun@gmail.com>
2024-03-12 10:01:49 +0100

    Initial commit

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Page de connexion</title>
+</head>
+<body>
+  <main>
+    <?php
+    ?>
+    <form method="post">
+      <input type="text" name="username" placeholder="Nom d'utilisateur">
+      <input type="password" name="password" placeholder="Mot de passe">
+      <button type="submit">Connexion</button>
+    </form>
+</body>
+</html>
